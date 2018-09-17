6750429c93b0d4e94d08f19e88e21d8cb78d763e
Bill Diamond <bill@billdiamond.com>
2018-09-17 11:33:48 -0400

    Add files via upload

--- scripts/config_version.rb
+++ scripts/config_version.rb
@@ -1,25 +1,23 @@
-#!/opt/puppetlabs/puppet/bin/ruby
 begin
   require 'rugged'
-  require 'socket'
-rescue LoadError
+rescue LoadError => e
   t = Time.new
   puts t.to_i
 else
+
   environmentpath = ARGV[0]
   environment     = ARGV[1]
 
-  # Get the hostname of the Puppet master compiling the catalog.
-  # Sometimes the hostname is the fqdn, so we'll take the first segment.
-  compiling_master = Socket.gethostname.split('.').first
-
-  # Get the path to the environment being compiled.
   repo = Rugged::Repository.discover(File.join(environmentpath, environment))
-  head = repo.head
+  head  = repo.head
 
-  # First 12 characters of the sha1 hash of the newest commit.
-  commit_id = head.target_id[0...11]
+  #sha1 hash of the newest commit
+  head_sha = head.target_id
 
-  # Show the compiling master, environment name, and commit ID.
-  puts "#{compiling_master}-#{environment}-#{commit_id}"
+  #the commit message associated the newest commit
+  commit = repo.lookup(head_sha)
+
+  #add something to find the remote url
+
+  puts head_sha 
 end
