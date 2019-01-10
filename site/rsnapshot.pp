b11beeee149195349693a26c2999769e3b64bcfb
Bill Diamond <bill@billdiamond.com>
2019-01-10 15:34:22 -0500

    Update rsnapshot.pp

--- site/rsnapshot.pp
+++ site/rsnapshot.pp
@@ -1,11 +1,9 @@
+
 class { '::rsnapshot':
-  hosts => {
-    'localhost' => {},
-    'razor.billdiamond.com'    => {
-      backup_defaults => false,
-      backup          => {
-        '/var/'       => './'
-      }
-    }
-  }
+    snapshot_root => '/var/backups/rsnapshot',
+    retains       => [  { 'daily'   => 7 }, { 'weekly'  => 4 }, { 'monthly' => 7 }, ],
+    crons         => {  'daily'   => { 'minute' => 30, 'hour' => 6, },
+                        'weekly'  => { 'minute' => 30, 'hour' => 5, 'weekday'  => 6 },
+                        'monthly' => { 'minute' => 30, 'hour' => 4, 'monthday' => 1 }, },
+    backups       => hiera('rsnapshot_backups'),
 }
