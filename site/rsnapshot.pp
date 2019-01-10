6fe4c3145dcb155371b1458d7323d677cf8b8fe1
Bill Diamond <bill@billdiamond.com>
2019-01-10 17:04:13 -0500

    Update rsnapshot.pp

--- site/rsnapshot.pp
+++ site/rsnapshot.pp
@@ -1,9 +1,3 @@
-
-class { '::rsnapshot':
-    snapshot_root => '/var/backups/rsnapshot',
-    retains       => [  { 'daily'   => 7 }, { 'weekly'  => 4 }, { 'monthly' => 7 }, ],
-    crons         => {  'daily'   => { 'minute' => 30, 'hour' => 6, },
-                        'weekly'  => { 'minute' => 30, 'hour' => 5, 'weekday'  => 6 },
-                        'monthly' => { 'minute' => 30, 'hour' => 4, 'monthday' => 1 }, },
-    backups       => hiera('rsnapshot_backups'),
+class { 'rsnapshot::server':
+  backup_path    => '/backups/rsnapshot'
 }
